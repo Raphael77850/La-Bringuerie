cff7ad6fcf249a586dcb9cacd473e4dd85c99d0a
Raphaël Streiff <raphaelstreiff93@gmail.com>
2025-02-14 09:45:49 +0100

    fix css

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,12 +1,13 @@
-import { AppBar } from "@mui/material";
+import { AppBar, Box } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { Link as RouterLink } from "react-router-dom";
-import letterB from "../../assets/images/IconeBM.png";
+import letterB from "../../assets/images/Logo-Officiel.png";
 
 const useStyles = makeStyles({
   appBar: {
     backgroundColor: "white !important",
     boxShadow: "none !important",
+    padding: "1rem",
   },
   container: {
     display: "flex !important",
@@ -28,15 +29,17 @@ export default function Header() {
 
   return (
     <AppBar position="static" className={classes.appBar}>
-      <RouterLink to="/">
-        <img src={letterB} alt="La Bringuerie" style={{ height: "2rem" }} />
-      </RouterLink>
-      <RouterLink
-        to="/about"
-        style={{ color: "orange", textDecoration: "none" }}
-      >
-        À propos
-      </RouterLink>
+      <Box className={classes.container}>
+        <RouterLink to="/">
+          <img src={letterB} alt="La Bringuerie" style={{ height: "3rem" }} />
+        </RouterLink>
+        <RouterLink
+          to="/about"
+          style={{ color: "orange", textDecoration: "none" }}
+        >
+          À propos
+        </RouterLink>
+      </Box>
     </AppBar>
   );
 }
