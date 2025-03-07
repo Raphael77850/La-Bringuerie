a5260ce3e86a4d5a5cf43f79e2e83a202399c24d
Raphaël Streiff <raphaelstreiff93@gmail.com>
2025-03-07 17:03:57 +0100

    config vite to accept .mp4 vidéo

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,13 +3,13 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ["**/*.MP4", "**/*.mp4", "**/*.Mp4"],
   server: {
-    port: 3000,
+    port: 3000, // Spécifie explicitement le port 3000
     proxy: {
       "/api": {
         target: "http://localhost:3310",
         changeOrigin: true,
-        secure: false,
       },
     },
   },
